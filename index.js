4a44c7681546b04fa45be5ecad70c6f1ca0af4f2
Nguyen Anh Tuan <tuananhnguyenasp@gmail.com>
2021-02-21 21:43:18 +0700

    Install Morgan

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 // Run Project: node index.js
 // Install Nodemon: npm i nodemon --save-dev
 // Run Project After Install Nodemon: npm start
+// Install Morgan: npm i morgan --save-dev
 
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+app.use(morgan('combined'));
+
 app.get('/tin-tuc', (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c = a + b;
+
   return res.send('Hello The Blog Website');
 });
 
