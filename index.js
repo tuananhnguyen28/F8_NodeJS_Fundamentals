59aef816ca9c32c8cde13f66274d3a7f69f6cf57
Nguyen Anh Tuan <tuananhnguyenasp@gmail.com>
2021-02-21 21:29:26 +0700

    Install Nodemon And Inspect Debug

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-// Install Syntax: npm install express
-// Run Syntax: node index.js
+// Install Express: npm install express
+// Run Project: node index.js
+// Install Nodemon: npm i nodemon --save-dev
+// Run After Install Nodemon: npm start
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/tin-tuc', (req, res) => {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
   return res.send('Hello The Blog Website');
 });
 
