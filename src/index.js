dace591c787add41196a4d5b645f14454ed3d33e
Nguyen Anh Tuan <tuananhnguyenasp@gmail.com>
2021-02-26 23:14:33 +0700

    Static Files And SCSS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 // Run Project After Install Nodemon: npm start
 // Install Morgan: npm i morgan --save-dev
 // Install Express Handlebars: npm i express-handlebars
+// Install Node Sass: npm i node-sass --save-dev
+// How to run Node Sass: npm run watch (configured in package.json)
+// How to configure Node Sass (running multiple folders): node-sass --watch <input:root folder> --output <output: roor folder>
 
 const path = require('path');
 const express = require('express');
@@ -11,6 +14,8 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
 app.use(morgan('combined'));
